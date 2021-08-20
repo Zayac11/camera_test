aec8b138e31dea6db61527cf0c5a8f9a61af6b82
Zayac11 <brunogrbstudios@gmail.com>
2021-08-20 14:49:52 +0300

    scanner added

--- src/components/Container/Scanner/Scanner.jsx
+++ src/components/Container/Scanner/Scanner.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
+import QrReader from 'react-qr-scanner'
+
 
 const Scanner = () => {
+
+    const [result, setResult] = useState('')
+
+    const previewStyle = {
+        height: 240,
+        width: 320,
+    }
+
+    const handleScan = (data) => {
+
+        setResult(data?.text)
+    }
+    const handleError = (err) => {
+        console.error(err)
+    }
+
     return (
         <div>
-            Scanner
+            <QrReader
+                // delay={1000}
+                style={previewStyle}
+                onError={handleError}
+                onScan={handleScan}
+            />
+            <p>{result}</p>
         </div>
     )
 }
