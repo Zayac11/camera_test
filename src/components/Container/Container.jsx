6185962e176e310e017b847b7fddc2c9ea6b9521
Zayac11 <brunogrbstudios@gmail.com>
2021-08-20 14:58:20 +0300

    Container fixed

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {NavLink, Redirect, Route} from 'react-router-dom'
+import {NavLink, Route} from 'react-router-dom'
 import Scanner from './Scanner/Scanner'
 import Photo from './Photo/Photo'
 import s from './Container.module.scss'
@@ -7,9 +7,8 @@ import s from './Container.module.scss'
 const Container = () => {
     return (
         <div className={s.container}>
-            <Route exact path={'/scanner'} render={() => <Scanner />} />
+            <Route exact path={'/'} render={() => <Scanner />} />
             <Route exact path={'/photo'} render={() => <Photo />} />
-            <Route exact path={'/'} render={() => <Redirect to={'/scanner'} />} />
 
             <div className={s.footer}>
                 <NavLink className={s.link} activeClassName={s.active} to={'/scanner'}>Сканер QR</NavLink>
